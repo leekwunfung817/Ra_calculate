22e3f0238e6a2f76f45196fa24fb17481b371a4c
ivan.lee <ivan.lee@neuralt.com>
2021-05-12 18:43:29 +0800

    Add KNN to calculate

---
+++ KNN_1.py
@@ -0,0 +1,25 @@
+from KNN_1_data import result,data,predict
+# https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=cmd
+command = '''
+
+pip3 install sklearn==0.0
+cd C:/Users/ivan.lee.PRIMECREATION/Documents/ivan/Projects source/Others/h/Ra_deploy/Script/Python
+python3 KNN_1.py
+
+
+'''
+
+# X = [[0], [1], [2], [3]]
+# y = [0, 0, 1, 1]
+
+from sklearn.neighbors import KNeighborsRegressor
+
+neigh = KNeighborsRegressor(n_neighbors=2)
+print(len(data),len(result))
+neigh.fit(data, result)
+# KNeighborsRegressor(...)
+# r1 = neigh.predict(data)
+r1 = neigh.predict(predict)
+for x in r1:
+	print(x)
+# [0.5]
